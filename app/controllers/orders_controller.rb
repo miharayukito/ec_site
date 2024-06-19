669480c44f450308390a705dec3535e4097783af
miharayukito <mihara2994@gmail.com>
2024-06-19 17:09:28 +0900

    Status追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,30 +3,40 @@ class OrdersController < ApplicationController
     def new
         @order = Order.new
         @book = Book.find(params[:format])
+
+        if @book.sold_out?
+            redirect_to products_path
+        end
     end
 
     def confirm
         @order = Order.new(order_params)
         @book = Book.find(order_params[:book_id])
+
+        if @book.sold_out?
+            redirect_to products_path
+        end
     end
 
     
     def create
         @order = Order.new(order_params) 
         if @order.save
-            redirect_to complete_orders_path, notice: '登録が完了しました。'
+            @book = Book.find(order_params[:book_id])
+            @book.sold_out!
+            redirect_to complete_orders_path
         else
             render :"confirm"
         end
     end
     
     def complete
-        CompleteMailer.complete_mail(current_user).deliver_now
+        CompleteMailer.complete_mail(current_user).deliver
     end
 
     private
 
     def order_params
-        params.require(:order).permit(:count, :address, :book_id)
+        params.require(:order).permit(:count, :address, :book_id, :status)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
     has_many :taggings
     has_many :tags,through: :taggings
+    has_one_attached :photo
+
+    enum status: {on_sale: 1, sold_out: 2}
+
+    def thumbnail
+        photo.variant(resize_to_fill:[200, 200])
+    end
 end
